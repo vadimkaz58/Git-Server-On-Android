fa8b5d70c8e0abacb592a8749d38fa22a2a2ed61
vadimkaz58 <vadim.kaz58@mail.ru>
2021-12-14 16:22:14 +0300

    Added setting and design

--- app/src/main/java/com/example/servergit/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/servergit/ui/profile/ProfileFragment.java
@@ -1,6 +1,5 @@
 package com.example.servergit.ui.profile;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,14 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.servergit.MainActivity;
@@ -31,6 +29,13 @@ public class ProfileFragment extends Fragment {
     private Button buttonLogIn;
     private String login;
     private String gitAddress;
+    private String password;
+    private TabHost tabHost;
+    private Button buttonSetting;
+    private Button buttonSettingBack;
+    private Button buttonLogOut;
+    private TextView textViewLogin;
+    private TextView textViewAddress;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,12 +48,30 @@ public class ProfileFragment extends Fragment {
         editTextAddress = root.findViewById(R.id.editTextAddress);
         this.root = root;
 
+        textViewLogin = root.findViewById(R.id.textViewLogin);
+        textViewAddress = root.findViewById(R.id.textViewAddress);
+
         buttonLogIn = root.findViewById(R.id.buttonLogIn);
         buttonLogIn.setOnClickListener(view -> {
-            logIn(view);
+            logIn();
         });
 
-        TabHost tabHost = root.findViewById(R.id.profileTabHost);
+        buttonSetting = root.findViewById(R.id.buttonSetting);
+        buttonSetting.setOnClickListener(view -> {
+            goToSetting();
+        });
+
+        buttonSettingBack = root.findViewById(R.id.buttonSettingBack);
+        buttonSettingBack.setOnClickListener(view -> {
+            goToProfile();
+        });
+
+        buttonLogOut = root.findViewById(R.id.buttonLogOut);
+        buttonLogOut.setOnClickListener(view -> {
+            logOut();
+        });
+
+        tabHost = root.findViewById(R.id.profileTabHost);
 
         tabHost.setup();
 
@@ -63,9 +86,17 @@ public class ProfileFragment extends Fragment {
         tabSpec.setIndicator("Profile");
         tabHost.addTab(tabSpec);
 
+        tabSpec = tabHost.newTabSpec("tag3");
+        tabSpec.setContent(R.id.tabSetting);
+        tabSpec.setIndicator("Setting");
+        tabHost.addTab(tabSpec);
+
         if (MainActivity.mSetting.getBoolean("is_logged", true)) {
             login = MainActivity.mSetting.getString("login", new String());
             gitAddress = MainActivity.mSetting.getString("gitAddress", new String());
+            password = MainActivity.mSetting.getString("password", new String());
+            textViewLogin.setText(login);
+            textViewAddress.setText(gitAddress);
             tabHost.setCurrentTab(1);
         } else {
             tabHost.setCurrentTab(0);
@@ -80,11 +111,11 @@ public class ProfileFragment extends Fragment {
         return root;
     }
 
-    private void logIn(View view) {
-        String login = String.valueOf(editTextLogin.getText());
-        String password = String.valueOf(editTextPassword.getText());
-        String address = String.valueOf(editTextAddress.getText());
-        if (login.isEmpty() || password.isEmpty() || address.isEmpty()) {
+    private void logIn() {
+        login = String.valueOf(editTextLogin.getText());
+        password = String.valueOf(editTextPassword.getText());
+        gitAddress = String.valueOf(editTextAddress.getText());
+        if (login.isEmpty() || password.isEmpty() || gitAddress.isEmpty()) {
             Toast toast = new Toast(root.getContext());
             toast.setText("Заполните все поля!");
             toast.setDuration(Toast.LENGTH_SHORT);
@@ -94,7 +125,23 @@ public class ProfileFragment extends Fragment {
             editor.putBoolean("is_logged", true).apply();
             editor.putString("login", login).apply();
             editor.putString("password", password).apply();
-            editor.putString("gitAddress", address).apply();
+            editor.putString("gitAddress", gitAddress).apply();
+            textViewLogin.setText(login);
+            textViewAddress.setText(gitAddress);
+            tabHost.setCurrentTab(1);
         }
+    }
+    private void goToSetting() {
+        tabHost.setCurrentTab(2);
+    }
+
+    private void goToProfile() {
+        tabHost.setCurrentTab(1);
+    }
+
+    private void logOut() {
+        SharedPreferences.Editor editor = MainActivity.mSetting.edit();
+        editor.putBoolean("is_logged", false).apply();
+        tabHost.setCurrentTab(0);
     }
 }
